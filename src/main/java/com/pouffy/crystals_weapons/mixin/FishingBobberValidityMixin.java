dab97c4079bb2f93c1abb234bf77191941c8e53a
pouffy <pouffygamer@outlook.com>
2023-04-18 19:14:04 +0100

    shit

---
+++ src/main/java/com/pouffy/crystals_weapons/mixin/FishingBobberValidityMixin.java
@@ -0,0 +1,41 @@
+package com.pouffy.crystals_weapons.mixin;
+
+import com.pouffy.crystals_weapons.common.tool.FishingRodSwordItem;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.FishingBobberEntity;
+import net.minecraft.item.FishingRodItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(FishingBobberEntity.class)
+public abstract class FishingBobberValidityMixin extends Entity {
+    @Shadow
+    public abstract PlayerEntity getPlayerOwner();
+
+    private FishingBobberValidityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+    @Inject(
+            method = "removeIfInvalid",
+            at = @At("HEAD"),
+            cancellable = true
+    )
+    private void removeIfInvalid(PlayerEntity playerEntity, CallbackInfoReturnable<Boolean> cir) {
+        ItemStack mainHandStack = playerEntity.getMainHandStack();
+        ItemStack offHandStack = playerEntity.getOffHandStack();
+
+        boolean mainHandHasRod = mainHandStack.getItem() instanceof FishingRodSwordItem;
+        boolean offHandHasRod = offHandStack.getItem() instanceof FishingRodSwordItem;
+
+        if (!playerEntity.isRemoved() && playerEntity.isAlive() && (mainHandHasRod || offHandHasRod) && this.squaredDistanceTo(playerEntity) <= 1024.0D) {
+            cir.setReturnValue(false);
+        }
+    }
+}
